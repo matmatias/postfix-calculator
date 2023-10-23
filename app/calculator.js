ecb96e4a306198259b8cf8e8dfb9cbb5bb952b44
SamuraX <matheusomr1@gmail.com>
2023-10-23 12:30:39 -0300

    Solution

--- app/calculator.js
+++ app/calculator.js
@@ -1,4 +1,51 @@
-exports.calculate = function(expression) {
-  console.log("test");
+exports.calculate = function (expression) {
+  const tokens = expression.split(" ");
+  const operandsQueue = [];
+  const operatorsStack = [];
+
+  for (let token of tokens) {
+    if (!isNaN(token)) {
+      operandsQueue.push(parseFloat(token));
+    }
+    if (isOperator(token)) {
+      operatorsStack.push(token);
+    }
+  }
+
+  while (operandsQueue.length > 1 && operatorsStack.length > 0) {
+    const operator = operatorsStack.pop();
+
+    const operand2 = operandsQueue.shift();
+    const operand1 = operandsQueue.shift();
+    const result = applyOperator(operand1, operand2, operator);
+
+    operandsQueue.push(result);
+  }
+
+  if (operandsQueue.length === 1) {
+    return operandsQueue[0];
+  }
+
   return 0;
+};
+
+function isOperator(token) {
+  if (token === "+" || token === "-" || token === "/" || token === "*") {
+    return true;
+  }
+
+  return false;
+}
+
+function applyOperator(operand1, operand2, operator) {
+  switch (operator) {
+    case "+":
+      operand1 + operand2;
+    case "-":
+      operand1 - operand2;
+    case "*":
+      operand1 * operand2;
+    case "/":
+      operand1 / operand2;
+  }
 }
